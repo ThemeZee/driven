25f488bec0c7ffd5db844cb0f2dcd275f1641188
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-04-20 14:26:51 +0200

    updated Footer template part

--- inc/patterns/footer-widgets.php
+++ inc/patterns/footer-widgets.php
@@ -5,8 +5,8 @@
 return array(
 	'title'      => __( 'Footer Widgets', 'driven' ),
 	'categories' => array( 'driven_layout' ),
-	'content'    => '<!-- wp:columns -->
-					<div class="wp-block-columns">
+	'content'    => '<!-- wp:columns {"style":{"spacing":{"padding":{"top":"2vw","bottom":"2vw"}}}} -->
+					<div class="wp-block-columns" style="padding-top:2vw;padding-bottom:2vw">
 
 						<!-- wp:column -->
 						<div class="wp-block-column">
@@ -14,8 +14,8 @@ return array(
 							<!-- wp:group {"className":"footer-widget"} -->
 							<div class="wp-block-group footer-widget">
 
-								<!-- wp:heading {"level":3} -->
-								<h3>' . __( 'About Driven', 'driven' ) . '</h3>
+								<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
+								<h3 class="has-white-color has-text-color has-large-font-size">' . __( 'About Driven', 'driven' ) . '</h3>
 								<!-- /wp:heading -->
 
 								<!-- wp:paragraph -->
@@ -34,8 +34,8 @@ return array(
 							<!-- wp:group {"className":"footer-widget"} -->
 							<div class="wp-block-group footer-widget">
 
-							<!-- wp:heading {"level":3} -->
-							<h3>' . __( 'Categories', 'driven' ) . '</h3>
+							<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
+							<h3 class="has-white-color has-text-color has-large-font-size">' . __( 'Categories', 'driven' ) . '</h3>
 							<!-- /wp:heading -->
 
 							<!-- wp:categories {"className":"is-style-flip-link-hover"} /-->
@@ -52,29 +52,11 @@ return array(
 							<!-- wp:group {"className":"footer-widget"} -->
 							<div class="wp-block-group footer-widget">
 
-							<!-- wp:heading {"level":3} -->
-							<h3>' . __( 'Archives', 'driven' ) . '</h3>
+							<!-- wp:heading {"level":3,"textColor":"white","fontSize":"large"} -->
+							<h3 class="has-white-color has-text-color has-large-font-size">' . __( 'Archives', 'driven' ) . '</h3>
 							<!-- /wp:heading -->
 
 							<!-- wp:archives {"className":"is-style-flip-link-hover"} /-->
-
-							</div>
-							<!-- /wp:group -->
-
-						</div>
-						<!-- /wp:column -->
-
-						<!-- wp:column -->
-						<div class="wp-block-column">
-
-							<!-- wp:group {"className":"footer-widget"} -->
-							<div class="wp-block-group footer-widget">
-
-							<!-- wp:heading {"level":3} -->
-							<h3>' . __( 'Tags', 'driven' ) . '</h3>
-							<!-- /wp:heading -->
-
-							<!-- wp:tag-cloud {"className":"is-style-flip-link-hover"} /-->
 
 							</div>
 							<!-- /wp:group -->
