258b15c81c37697538787cef6fd4a393f531c174
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-05-18 10:21:11 +0200

    register block styles in functions.php

--- functions.php
+++ functions.php
@@ -151,5 +151,43 @@ function driven_register_block_pattern_categories() {
 add_action( 'init', 'driven_register_block_pattern_categories', 9 );
 
 
-// Add block styles.
-require get_template_directory() . '/inc/block-styles.php';
+/**
+ * Registers block styles.
+ *
+ * @return void
+ */
+function driven_register_block_styles() {
+
+	// Register Main Navigation block style.
+	register_block_style( 'core/navigation', array(
+		'name'         => 'main-navigation',
+		'label'        => esc_html__( 'Main Navigation', 'driven' ),
+		'style_handle' => 'driven-stylesheet',
+	) );
+
+	// Register Underlined Heading block style.
+	$underlined_heading_style = array(
+		'name'         => 'underlined-heading',
+		'label'        => esc_html__( 'Underlined', 'driven' ),
+		'style_handle' => 'driven-stylesheet',
+	);
+
+	register_block_style( 'core/heading', $underlined_heading_style );
+	register_block_style( 'core/post-title', $underlined_heading_style );
+	register_block_style( 'core/query-title', $underlined_heading_style );
+
+	// Register Flip Link Hover block style.
+	$link_hover_style = array(
+		'name'         => 'flip-link-hover',
+		'label'        => esc_html__( 'Flip Link Hover', 'driven' ),
+		'style_handle' => 'driven-stylesheet',
+	);
+
+	register_block_style( 'core/archives', $link_hover_style );
+	register_block_style( 'core/categories', $link_hover_style );
+	register_block_style( 'core/latest-posts', $link_hover_style );
+	register_block_style( 'core/page-list', $link_hover_style );
+	register_block_style( 'core/tag-cloud', $link_hover_style );
+	register_block_style( 'core/site-title', $link_hover_style );
+}
+add_action( 'init', 'driven_register_block_styles', 9 );
