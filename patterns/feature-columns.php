697f3ce1f8eeec6afc4c5d80fbe35d9ed6c74cad
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-05-18 14:41:52 +0200

    updated Features block patterns

--- patterns/feature-columns.php
+++ patterns/feature-columns.php
@@ -6,8 +6,8 @@
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"light-gray","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
-<div class="wp-block-group alignfull has-light-gray-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
+<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
 
 	<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
 	<div class="wp-block-columns alignwide are-vertically-aligned-center">
--- patterns/feature-grid.php
+++ patterns/feature-grid.php
@@ -6,14 +6,14 @@
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"},"blockGap":"3.5vw"}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
 <div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
 
-	<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"bottom":"3.5vw"}}}} -->
-	<h2 class="has-text-align-center" style="margin-bottom:3.5vw"><?php _e( 'Why choose us?', 'driven' ); ?></h2>
+	<!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="has-text-align-center"><?php _e( 'Why choose us?', 'driven' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:columns {"align":"wide"} -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"3.5vw"}}} -->
 	<div class="wp-block-columns alignwide">
 
 		<!-- wp:column -->
@@ -22,8 +22,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>1</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>1</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -42,8 +42,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>2</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>2</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -62,8 +62,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>3</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>3</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -79,7 +79,7 @@
 		</div>
 		<!-- /wp:columns -->
 
-	<!-- wp:columns {"align":"wide"} -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"3.5vw"}}} -->
 	<div class="wp-block-columns alignwide">
 
 		<!-- wp:column -->
@@ -88,8 +88,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>4</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>4</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -108,8 +108,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>5</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>5</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -128,8 +128,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px"><strong>6</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"0.1"}},"textColor":"primary"} -->
+				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:0.1"><strong>6</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
