3cb419a2361194dc18921c2ced37ca916f9cc9a5
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-05-19 14:17:19 +0200

    added Services block patterns

--- patterns/services-ladder.php
+++ patterns/services-ladder.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Title: Services Columns
- * Slug: driven/services-columns
+ * Title: Services Ladder
+ * Slug: driven/services-ladder
  * Categories: driven_services
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"},"blockGap":"3.5vw"}},"backgroundColor":"white","layout":{"contentSize":"1200px","wideSize":"1200px"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
 <div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
 
-	<!-- wp:heading {"className":"is-style-underlined-heading"} -->
-	<h2 class="is-style-underlined-heading"><?php _e( 'What we offer', 'driven' ); ?></h2>
+	<!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="has-text-align-center"><?php _e( 'What we offer', 'driven' ); ?></h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"2rem"}}} -->
@@ -19,8 +19,8 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:heading {"level":3} -->
-			<h3><?php _e( 'Service One', 'driven' ); ?></h3>
+			<!-- wp:heading {"level":3,"className":"is-style-underlined-heading"} -->
+			<h3 class="is-style-underlined-heading"><?php _e( 'Service One', 'driven' ); ?></h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
@@ -33,8 +33,12 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:heading {"level":3} -->
-			<h3><?php _e( 'Service Two', 'driven' ); ?></h3>
+			<!-- wp:spacer {"height":"30px"} -->
+			<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+			<!-- /wp:spacer -->
+
+			<!-- wp:heading {"level":3,"className":"is-style-underlined-heading"} -->
+			<h3 class="is-style-underlined-heading"><?php _e( 'Service Two', 'driven' ); ?></h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
@@ -47,8 +51,12 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:heading {"level":3} -->
-			<h3><?php _e( 'Service Three', 'driven' ); ?></h3>
+			<!-- wp:spacer {"height":"60px"} -->
+			<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+			<!-- /wp:spacer -->
+
+			<!-- wp:heading {"level":3,"className":"is-style-underlined-heading"} -->
+			<h3 class="is-style-underlined-heading"><?php _e( 'Service Three', 'driven' ); ?></h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
