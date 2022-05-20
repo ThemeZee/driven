d98135f4548ebf549bc3e63a5b770e762d6d19ce
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-05-20 11:42:10 +0200

    added dark layouted Portfolio block patterns

--- patterns/portfolio-list-dark.php
+++ patterns/portfolio-list-dark.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Title: Portfolio List
- * Slug: driven/portfolio-list
+ * Title: Portfolio List Dark
+ * Slug: driven/portfolio-list-dark
  * Categories: driven_portfolio
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
-<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"dark-gray","textColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
+<div class="wp-block-group alignfull has-white-color has-dark-gray-background-color has-text-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
 
 	<!-- wp:media-text {"mediaType":"image","verticalAlignment":"center"} -->
 	<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center">
