751835b7b7e1022e24fe123fc2058187ffab13a1
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-05-23 16:31:23 +0200

    added Hero block patterns

--- patterns/hero-columns-dark.php
+++ patterns/hero-columns-dark.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Title: Hero Section
- * Slug: driven/hero-section
+ * Title: Hero Columns Dark
+ * Slug: driven/hero-columns-dark
  * Categories: driven_hero
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
-<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"dark-gray","textColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
+<div class="wp-block-group alignfull has-white-color has-dark-gray-background-color has-text-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
 
 	<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
 	<div class="wp-block-columns alignwide are-vertically-aligned-center" style="margin-bottom:0px">
@@ -19,15 +19,15 @@
 			<h1 class="is-style-underlined-heading" style="margin-top:0px"><?php _e( 'Pursue your passion <br>All dreams can come true', 'driven' ); ?></h1>
 			<!-- /wp:heading -->
 
-			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+			<!-- wp:paragraph {"fontSize":"medium"} -->
+			<p class="has-medium-font-size">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:buttons {"className":"is-style-default"} -->
 			<div class="wp-block-buttons is-style-default">
 
 				<!-- wp:button -->
-				<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Start now', 'driven' ); ?></a></div>
+				<div class="wp-block-button"><a class="wp-block-button__link"><?php _e( 'Join now', 'driven' ); ?></a></div>
 				<!-- /wp:button -->
 
 				<!-- wp:button -->
@@ -43,8 +43,10 @@
 		<!-- wp:column {"verticalAlignment":"center"} -->
 		<div class="wp-block-column is-vertically-aligned-center">
 
-			<!-- wp:image -->
-			<figure class="wp-block-image"><img alt=""/></figure>
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/default-image.png" alt="">
+			</figure>
 			<!-- /wp:image -->
 
 		</div>
